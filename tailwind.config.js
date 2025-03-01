0ad0febd217030531970979986ac0bad97c6efd1
Kauê <kauevecchia@hotmail.com>
2025-02-28 21:45:01 -0300

    chore: add custom color palette to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-blue': "#1E6F9F",
+        'blue': "#4EA8DE",
+        'dark-purple': "#5E60CE",
+        'purple': "#8284FA",
+        'danger': "#E25858",
+        gray: {
+          700: "#0D0D0D",
+          600: "#1A1A1A",
+          500: "#262626",
+          400: "#333333",
+          300: "#808080",
+          200: "#D9D9D9",
+          100: "#F2F2F2",
+        }
+      }
+    },
   },
   plugins: [],
 }
