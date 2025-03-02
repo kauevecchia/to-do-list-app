20781fc16e316946b5bc7b342c15733602ee384c
Kauê <kauevecchia@hotmail.com>
2025-03-01 23:44:51 -0300

    style: add layout and background styles to App container

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Header } from './components/Header'
+import { NewTask } from './components/NewTask'
 
 export function App() {
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center mih-h-screen bg-gray-600'>
       <Header />
 
+      
+      
       
     </div>
   )
