58ed559cd9440387d0fea1d8eabd71b289c9dedf
Kauê <kauevecchia@hotmail.com>
2025-03-02 03:58:55 -0300

    style: add flex and min-h-screen to center content

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import { Header } from './components/Header'
-import { NewTask } from './components/NewTask'
 
 export function App() {
   return (
-    <div className='flex flex-col items-center justify-center mih-h-screen bg-gray-600'>
+    <div className='flex flex-col items-center justify-center min-h-screen bg-gray-600'>
       <Header />
 
       
