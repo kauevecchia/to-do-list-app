937e9e89b7dedfdfbe3b1f632fe07c27d6f5edc9
Kauê <kauevecchia@hotmail.com>
2025-03-02 22:40:27 -0300

    feat: add state and handleAddTask function for task input and validation

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -5,8 +5,18 @@ interface AddTaskProps {
 }
 
 export function AddTask({ addTask }: AddTaskProps) {
+    const [taskText, setTaskText] = useState("")
+    const [inputError, setInputError] = useState(false)
 
-    
+    const handleAddTask = () => {
+        if (taskText.trim()) {
+            setInputError(true)
+            return
+        }
+        addTask(taskText)
+        setTaskText("")
+    }
+
     return (
         <div className='flex items-center justify-center gap-4 mt-8 w-full'>
             <input type="text" placeholder="Adicione uma nova tarefa" className='p-4 bg-gray-500 rounded-md placeholder-gray-300 flex-grow border-2 border-transparent focus:border-sky-600 focus:border-2 focus:outline-none transition duration-200 text-white'/>
